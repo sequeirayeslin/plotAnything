3d087e411bbfee0c9637d6c82e18eb0b9c7c752b
sequeirayeslin <55938233+sequeirayeslin@users.noreply.github.com>
2021-10-15 15:19:51 +0530

    Update demo_plot_a_sine.py

--- demo_plot_a_sine.py
+++ demo_plot_a_sine.py
@@ -1,12 +1,8 @@
 
 import plotAnything as pa
-from math import sin, pi
+from math import sin
 
 pa.drawGraph()
 
-f = 2
-A = 2
-sin_2_Hz = lambda x: A * sin(2*pi*f*x)
-
-pa.drawFun(sin_2_Hz, -2, 2, autoScaleX= True, autoScaleY= True)
+pa.drawFun(sin, -3, 3, autoScaleX= True, autoScaleY= True)
 input()
